086c348871a12e4f5f4722235a92e6fb5fac8f78
Abdelhalim <haleem_ah1998@yahoo.com>
2021-05-24 17:36:30 +0200

    database connection

--- src/dbConnect.ts
+++ src/dbConnect.ts
@@ -2,15 +2,19 @@ import { createConnection } from "typeorm"
 import 'dotenv/config'
 import { post } from "./entity/post"
 import { project } from "./entity/project"
+//postgres://bgferstsbiefeu:088a89164fe9a2fa8dd1e84c2fb32d495e243fa47896157c2c4cb45536eb2e2a@ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/d2tf5botks36mn
 
 const conn = async () => {
     await createConnection({
         type: "postgres",
-        database: "nasa",
-        username: "postgres",
+        database: process.env.DB_NAME,
+        host: process.env.DB_URL,
+        port: 5432,
+        username: process.env.DB_NAME,
         password: process.env.DB_PASS,
         logging: true,
         synchronize: true,
+        ssl: true,
         entities: [post, project],
     })
 }
