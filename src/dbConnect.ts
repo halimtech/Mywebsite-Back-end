4905b4ab3ea9bc419a48d9a34f19c791f82cb1ed
Abdelhalim <haleem_ah1998@yahoo.com>
2021-06-02 13:49:23 +0200

    delete non important comments

--- src/dbConnect.ts
+++ src/dbConnect.ts
@@ -2,8 +2,7 @@ import { createConnection } from "typeorm"
 import 'dotenv/config'
 import { post } from "./entity/post"
 import { project } from "./entity/project"
-//postgres://bgferstsbiefeu:088a89164fe9a2fa8dd1e84c2fb32d495e243fa47896157c2c4cb45536eb2e2a@ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/d2tf5botks36mn
-//postgres://xzbvqaitsifnsk:@ec2-54-228-174-49.eu-west-1.compute.amazonaws.com:5432/decl3pamrq7i3o
+
 const conn = async () => {
     try {
         await createConnection({
@@ -13,7 +12,6 @@ const conn = async () => {
             port: 5432,
             username: process.env.DB,
             password: process.env.DB_PASS,
-            // url: "postgres://xzbvqaitsifnsk:ade15893c05c0ef7fdafaa638f037a99c800d549ba5d8fa97c5e3f5fd03f2477@ec2-54-228-174-49.eu-west-1.compute.amazonaws.com:5432/decl3pamrq7i3o",
             logging: true,
             synchronize: true,
             ssl: {
@@ -27,17 +25,7 @@ const conn = async () => {
         console.log(error);
 
     }
-    // await createConnection({
-    //     type: "postgres",
-    //     database: "nasa",
-    //     host: "localhost",
-    //     port: 5432,
-    //     username: "postgres",
-    //     password: "overwatch2014",
-    //     logging: true,
-    //     synchronize: true,
-    //     entities: [post, project],
-    // })
+
 
 }
 
